a31778107c75554290713a3ff26ea2b7830a48b1
Martin Asenov <m.asenov5752@gmail.com>
2019-08-21 22:35:14 +0300

    identity front-end changes applied

---
+++ Web/Plant-A-Plant.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Plant_A_Plant.Data;
+using Plant_A_Plant.Data.Models;
+
+[assembly: HostingStartup(typeof(Plant_A_Plant.Web.Areas.Identity.IdentityHostingStartup))]
+namespace Plant_A_Plant.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
